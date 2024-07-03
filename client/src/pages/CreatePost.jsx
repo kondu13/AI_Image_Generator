c3c7f16eeebc6d4fcb12389874c6e747b0cc2308
Karthik Kondu <kondukarthik@gmail.com>
2024-07-03 23:37:59 +0530

    updated api

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if(form.prompt){
       try{
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle',{
+        const response = await fetch('https://ai-image-generator-7a7h.onrender.com/api/v1/dalle',{
           method: 'POST',
           headers: {
             'Content-Type' : 'application/json',
@@ -50,7 +50,7 @@ const CreatePost = () => {
     if(form.prompt && form.photo && form.name){
       setLoading(true);
       try{
-        const response = await fetch('http://localhost:8080/api/v1/post',{
+        const response = await fetch('https://ai-image-generator-7a7h.onrender.com/api/v1/post',{
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
